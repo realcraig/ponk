c05587c55cb495a25304085898106e0737a977e4
Craig Robinson <realcraig@gmail.com>
2024-08-07 11:14:30 -0700

    added sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrthographicCamera } from "@react-three/drei";
 import { useState, useEffect, useRef } from "react";
+import {Howl, Howler} from 'howler';
 import { Paddle } from "./Paddle";
 import Court from "./Court";
 const ballRadius = 0.15;
@@ -12,12 +13,17 @@ function getRandomFloat(min, max) {
 
 function getRandomVelocity() {
   const angle = getRandomFloat(-Math.PI / 4, Math.PI / 4);
-  const speed = (Math.random() < 0.5 ? -1 : 1) * getRandomFloat(0.04, 0.05);
+  const speed = (Math.random() < 0.5 ? -1 : 1) * getRandomFloat(0.05, 0.06);
 
   return [speed * Math.cos(angle), speed * Math.sin(angle), 0];
 }
 
 function Ball({ courtRef, leftPaddleRef, rightPaddleRef }) {
+
+  // Setup the new Howl.
+  const plinkSound = new Howl({
+    src: ['/sounds/plink.mp3']
+  });
   
   const [ballPosition, setBallPosition] = useState([0, 0, 0]);
   const [ballVelocity, setBallVelocity] = useState([0, 0, 0]);
@@ -48,18 +54,22 @@ function Ball({ courtRef, leftPaddleRef, rightPaddleRef }) {
       // Check for collision with top wall
       if (ballPosition[1] + ballRadius >= courtRef.current.getTop()) {
         setBallVelocity([ballVelocity[0], -Math.abs(ballVelocity[1]), ballVelocity[2]]);
+        plinkSound.play();
       }
       // Check for collision with bottom wall
       else if (ballPosition[1] - ballRadius <= courtRef.current.getBottom()) {
         setBallVelocity([ballVelocity[0], Math.abs(ballVelocity[1]), ballVelocity[2]]);
+        plinkSound.play();
       }
       // Check for collision with left paddle
       else if (leftPaddleRef.current.intersectsPoint([ballPosition[0]-ballRadius, ballPosition[1], ballPosition[2]])) {
         setBallVelocity(leftPaddleRef.current.getDeflectionVelocity(ballPosition, ballVelocity));
+        plinkSound.play();
       }
       // Check for collision with right paddle
       else if (rightPaddleRef.current.intersectsPoint([ballPosition[0]+ballRadius, ballPosition[1], ballPosition[2]])) {
         setBallVelocity(rightPaddleRef.current.getDeflectionVelocity(ballPosition, ballVelocity));
+        plinkSound.play();
       }
     
     setBallPosition([ballPosition[0] + ballVelocity[0], ballPosition[1] + ballVelocity[1], ballPosition[2] + ballVelocity[2]]);    
@@ -79,6 +89,9 @@ function App() {
   const leftPaddleRef = useRef();
   const rightPaddleRef = useRef();
   const courtRef = useRef();
+
+  
+
 
   return (
     <Canvas style={{ background: "#000" }}>
